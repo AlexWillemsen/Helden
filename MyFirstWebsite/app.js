12a87334b1d028e30a1906b7829efa38f07caf98
AlexWillemsen <a.willemsen64@gmail.com>
2023-06-06 18:53:22 +0200

    .

--- MyFirstWebsite/app.js
+++ MyFirstWebsite/app.js
@@ -13,3 +13,17 @@ function displayMenu()
 
     elementId.style.display == "block" ? elementId.style.display = "none" : elementId.style.display = "block";
 }
+
+var i, checkboxes = document.querySelectorAll('input[type=checkbox]');
+
+function save() {
+    for (i = 0; i < checkboxes.length; i++) {
+        localStorage.setItem(checkboxes[i].value, checkboxes[i].checked); 
+    }
+}
+
+function load_() {
+    for (i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].checked = localStorage.getItem(checkboxes[i].value) === 'true' ? true:false;
+    }
+} 
